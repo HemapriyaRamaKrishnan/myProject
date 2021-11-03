e4d0b8abb24e2da64defee5c99f76b7c52b0f93f
hemapriya2001 <hemapriya@twilightsoftwares.com>
2021-11-03 19:50:05 +0530

    Fix:Form Patch value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
-import { RxwebValidators } from '@rxweb/reactive-form-validators';
-
+import { password, RxwebValidators } from '@rxweb/reactive-form-validators';
 
 @Component({
   selector: 'app-root',
@@ -19,20 +18,29 @@ export class AppComponent  {
   constructor(private formBuilder:FormBuilder) { }
 
 ngOnInit(): void {
-  this.signinForm = this.formBuilder.group(
-    {
-      FirstName: ['',Validators.required],
-      LastName: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
-      password:['', [Validators.required, Validators.minLength(8)]],
-      confirmPassword: ['',Validators.required],
-      phonenumber:['',[ Validators.required, RxwebValidators.pattern({expression:{'onlyDigit': /^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/} })]], 
-      country: ['',Validators.required],
-      food: ['',Validators.required],
-      check:['',Validators.required]
-  },
-  );
+  this.createForm();
+  console.log('value', this.signinForm.value);
+  this.signinForm.setValue({
+    FirstName :'Hema',
+    LastName : 'Priya',
+    email :'hema@123gmail.com',
+    password:'12345678',
+    confirmPassword:'12345678',
+    phonenumber:'8097898058',
+    country: 'India',
+    gender:'female',
+    check: 'true'
+  });
+  console.log(this.signinForm.value);
+
+  this.signinForm.patchValue({
+    FirstName:'Hemapriya',
+    LastName: 'Ramakrishnan'
+
+  })
+  
 }
+
 
  get f()
  {
@@ -40,8 +48,28 @@ ngOnInit(): void {
 }
 onSubmit()
 {
-  console.log("submitted");
+  console.log(this.signinForm);
   this.submitted=true;
+  
+  }
+  createForm(){
+    this.signinForm = this.formBuilder.group(
+      {
+        FirstName: ['',Validators.required],
+        LastName: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+        password:['', [Validators.required, Validators.minLength(8)]],
+        confirmPassword: ['',[Validators.required,Validators.minLength(8)]],
+        phonenumber:['',[ Validators.required, RxwebValidators.pattern({expression:{'onlyDigit': /^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/} }),]], 
+        country: ['',Validators.required],
+        gender: ['',Validators.required],
+        check:['',Validators.required]
+    },
+    );
+  }
+  onReset() {
+    this.submitted = false;
+    this.signinForm.reset();
+  }
 }
 
-}
